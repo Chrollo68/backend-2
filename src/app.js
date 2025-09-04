5c17c24d553af010a5acbfa1af26284bd13ea895
Chrollo68 <tejasredkar53@gmail.com>
2025-09-04 19:25:01 +0530

    1st commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import authRoutes from './routes/authRoutes.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/auth', authRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Backend Auth API running...');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
